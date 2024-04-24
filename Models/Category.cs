018399141dc60270334f520b56e3af245d651a3c
Johnny Saniat <Jcsaniat@gmail.com>
2024-04-23 20:21:59 -0500

    Posts

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@ namespace FanficBE.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Label { get; set; }
+        public string? Label { get; set; }
         public ICollection<Post> Posts { get; set; }
     }
 }
