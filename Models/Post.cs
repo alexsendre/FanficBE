813b864497a33dad1f862d1fa4daa37dc57b43cb
Johnny Saniat <Jcsaniat@gmail.com>
2024-05-04 13:09:15 -0500

    Updated Search Functionality

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,11 +4,11 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; } // Navigation property for the associated user
         public string Title { get; set; }
         public string Content { get; set; }
         public int CategoryId { get; set; }
         public ICollection<Category> Categories { get; set; }
-        public ICollection<User> Users { get; set; }
         public ICollection<Comment> Comments { get; set; }
     }
 }
